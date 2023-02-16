4a21100d7b9133acfc085e81265a8db8b0784837
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-02-15 23:08:23 -0400

    pasofinal

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,15 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+
+const tercero = document.querySelector("#tercer-num");
+const cuarto = document.querySelector("#cuarto-num");
+const formMult = document.querySelector("#multiplicar-form");
+const divMult = document.querySelector("#resultado-mult");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +18,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+formMult.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const tercerNum = Number.parseInt(tercero.value);
+  const cuartoNum = Number.parseInt(cuarto.value);
+
+  divMult.innerHTML = "<p>" + multiplicar(tercerNum, cuartoNum) + "</p>";
 });
